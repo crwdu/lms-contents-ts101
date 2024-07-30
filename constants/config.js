d94be41b88971edf98ffd0801e7df92c3c7645f7
dmhuh <dami@vanillacoding.co>
2024-07-30 19:28:49 +0900

    Update course path

--- constants/config.js
+++ constants/config.js
@@ -1,5 +1,5 @@
 // NOTE: 필요하다면, 하단에 과목 구분 코드를 변경해주세요. (예: js001, js101, js102, ...)
-const COURSE_SLUG = "beginners-web-handbook";
+const COURSE_SLUG = "prep";
 const DEPLOYMENT_ENVS = ["preview", "production", "development"];
 
 module.exports = {
